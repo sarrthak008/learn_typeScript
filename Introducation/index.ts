febebe8e39353335559f8cc73ce0c76e22f76223
sarrthak008 <sarthaknavale08@gmail.com>
2025-02-27 10:56:49 +0530

    introducation to typescript

---
+++ Introducation/index.ts
@@ -0,0 +1,18 @@
+
+let num:number = 10;
+console.log(num);
+
+//in js we change the we chane the value type of num but we cant change in TS
+
+/*
+  in js
+  let num =10;
+  num = "hello";
+  is valid but invalid in TS
+*/
+
+
+//run .ts file using   ""tsc index.ts""  command
+//run .js file using   ""node index.js""  command
+
+// tsc = ts = typescript c = compiler
