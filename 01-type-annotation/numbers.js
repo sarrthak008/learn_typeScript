e298769b7621d26a637124849b5fb7bf951692ed
sarrthak008 <sarthaknavale08@gmail.com>
2025-02-28 12:59:44 +0530

    learn number type in ts

---
+++ 01-type-annotation/numbers.js
@@ -0,0 +1,6 @@
+var myFevNumber = 10;
+var myBirthDate = 23;
+var myAge = 25;
+var avg = 12.3;
+var mathFormula = Math.sqrt(16);
+//we define number in these forms .....
---
+++ 01-type-annotation/numbers.ts
@@ -0,0 +1,7 @@
+let myFevNumber:number = 10;
+let myBirthDate:number=23;
+let myAge:number=25;
+let avg:number=12.3;
+let mathFormula:number = Math.sqrt(16)
+
+//we define number in these forms .....
